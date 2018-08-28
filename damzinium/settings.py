22a0535016d05ca009c0a3b9e52fd1a00c310059
JoJo Thmpsn <mnnlthmpsn@outlook.com>
2018-08-28 11:47:36 +0000

    sendgrid

--- damzinium/settings.py
+++ damzinium/settings.py
@@ -147,6 +147,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_HOST = os.environ["EMAIL_HOST"]
+SENDGRID_API = os.environ["SENDGRID_API"]
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_PORT = 587
