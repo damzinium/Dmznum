0746834d3da815d20f3c63a1dceaf2a10c0f22fd
mnnlthmpsn <mnnlthmpsn@outlook.com>
2018-10-08 08:07:24 +0000

    homepage redirected and pipenv removed

--- damzi/views.py
+++ damzi/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.contrib import auth
 
 # Create your views here.
 def index(request):
     auth.logout(request)
-    return render_to_response('accounts/ots.html')
+    return redirect('accounts:login')
