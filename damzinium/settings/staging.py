d9ca184c97cf242373fe4bb60a0e8c90de3a077c
John Kennedy <jimmyn341@gmail.com>
2018-11-17 16:40:02 +0000

    fixing ...

--- damzinium/settings/staging.py
+++ damzinium/settings/staging.py
@@ -6,18 +6,7 @@ SECRET_KEY = '1@!@#$kjsk#$%8923@kalkj,2TThSk@344#!@3$5#@()*(*&(*('
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['staging.damzinium.herokuapp.com', ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MIDDLEWARE += [
-	'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+ALLOWED_HOSTS = []
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
