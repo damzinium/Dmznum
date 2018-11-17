9e725da0bdcadda871116aeda4504df9bbf77348
John Kennedy <jimmyn341@gmail.com>
2018-11-17 16:11:56 +0000

    fixing problems with ckeditor

--- damzinium/settings/production.py
+++ damzinium/settings/production.py
@@ -12,12 +12,12 @@ MIDDLEWARE += [
 	'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_DIR = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
