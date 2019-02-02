d767ac9d50e080b17de4e6a46824bbeb81217cea
John <JKAkabo@github.com>
2019-02-02 22:20:43 +0000

    fixed bug

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from django.contrib.auth.decorators import permission_required, login_required
+from django.contrib.auth.decorators import permission_required, login_required, user_passes_test
 from . import views
 
 app_name = 'kitchen'
 
 urlpatterns = [
 # path('', permission_required('perm')(views.PostCreate.as_view()), name='add'),
-path('', login_required(views.ShowKitchen), name='index'),
-path('course/', login_required(views.PostCreate.as_view()), name='add'),
-path('ugrc/', login_required(views.UgrcPostCreate.as_view()), name='ugrc_add'),
+path('', user_passes_test(lambda u:u.is_staff)(login_required(views.ShowKitchen)), name='index'),
+path('course/', user_passes_test(lambda u:u.is_staff)(login_required(views.PostCreate.as_view())), name='add'),
+path('ugrc/', user_passes_test(lambda u:u.is_staff)(login_required(views.UgrcPostCreate.as_view())), name='ugrc_add'),
 ]
