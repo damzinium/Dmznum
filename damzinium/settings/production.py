bbf6ec1d5ce9914ca1e6a9744c182e9063e10a54
John Kennedy Akabo <akabojohnkennedy@gmail.com>
2019-03-23 11:24:37 +0000

    resolved problems with csrf_token

--- damzinium/settings/production.py
+++ damzinium/settings/production.py
@@ -6,13 +6,12 @@ SECRET_KEY = os.environ.get('SEC_PASS')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
 
-MIDDLEWARE += [
-	
-]
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
