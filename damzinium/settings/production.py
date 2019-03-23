e36f027f5c79f706204cd083c9f80cd15249d682
John Kennedy Akabo <akabojohnkennedy@gmail.com>
2019-03-23 10:59:29 +0000

    resolved problems with csrf_token

--- damzinium/settings/production.py
+++ damzinium/settings/production.py
@@ -13,7 +13,7 @@ MIDDLEWARE += [
 ]
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
